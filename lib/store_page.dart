beb551a03abfdd777c027511fc43911601d6bcf5
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-11 15:38:45 +0900

    add: UI 그리기

--- lib/store_page.dart
+++ lib/store_page.dart
@@ -5,6 +5,42 @@ class StorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/bag.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/shoes.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
